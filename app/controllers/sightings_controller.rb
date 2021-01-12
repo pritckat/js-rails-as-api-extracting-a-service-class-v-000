56eddf4eacc4bdb50a505291e125bb908d1c0cb6
Katrina Pritchard <pritckat@gmail.com>
2021-01-12 18:14:00 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -5,8 +5,9 @@ class SightingsController < ApplicationController
     render json: sightings.to_json(:include => {
       :bird => {:only => [:name, :species]},
       :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at]")
+    }, :except => [:updated_at])
   end
+
     def show
         @sighting = Sighting.find(params[:id])
         render json: @sighting.to_json(:include => {
